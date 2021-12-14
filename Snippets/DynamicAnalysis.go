a6e6187ef674d6bc6c187717cd0bd5dab12550c1
Enea Guidi <guidienea@gmail.com>
2021-12-14 22:58:05 +0100

    Improves Subsection 3.1.1

--- Snippets/DynamicAnalysis.go
+++ Snippets/DynamicAnalysis.go
@@ -6,30 +6,28 @@ import (
 	"time"
 )
 
-type msg struct {
+type payload struct {
 	data      int
 	timestamp int64
 }
 
-func worker(incoming, outgoing chan msg) {
+func worker(incoming chan int, outgoing chan payload) {
 	for msg := range incoming {
-		// ...do something with the message then sends
-		// back the results on the out channel
-		outgoing <- doSometing(msg)
+		// Sends back the results on the out channel
+		outgoing <- payload{msg + 1, time.Now().Unix()}
 	}
 }
 
 func main() {
-	// Creates the channles needed
-	in, out := make(chan msg, 10), make(chan msg, 10)
+	// Creates the channels
+	in, out := make(chan int, 10), make(chan payload, 10)
 	// Starts the worker processes
 	go worker(in, out)
 	go worker(in, out)
 	// Infinite loop
 	for {
-		newMsg := msg{rand.Int(), time.Now().Unix()}
-		in <- newMsg
+		in <- rand.Int()
 		res := <-out
-		fmt.Printf("Received %d at :%d", res.data, res.timestamp)
+		fmt.Printf("Received %d at %d \n", res.data, res.timestamp)
 	}
 }
